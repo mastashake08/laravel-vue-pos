cfb2bc34c2c48be1720e3f2c46c09849ce324e24
Jyrone Parker <jyrone.parker@gmail.com>
2018-04-27 16:52:29 -0400

    controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -41,52 +41,5 @@ class HomeController extends Controller
       return view('send')->with($with);
     }
 
-    public function sendPayment(Request $request,$id){
-      $user = \App\User::findOrFail($id);
-      try {
-
-        \Stripe\Stripe::setApiKey($user->secret_key);
-        // Use Stripe's library to make requests...
-        $token = \Stripe\Token::create(array(
-          "card" => array(
-            "number" => $request->pay['details']['cardNumber'],
-            "exp_month" => $request->pay['details']['expiryMonth'],
-            "exp_year" => $request->pay['details']['expiryYear'],
-            "cvc" => $request->pay['details']['cardSecurityCode']
-          )
-        ));
-        $charge = \Stripe\Charge::create(array(
-          "amount" => $request->amount,
-          "currency" => "usd",
-          "source" => $token,
-        ));
-
-        return response()->json([
-          'success' => true
-        ]);
-      } catch(\Stripe\Error\Card $e) {
-        // Since it's a decline, \Stripe\Error\Card will be caught
-        return response()->json($e->getJsonBody());
-      } catch (\Stripe\Error\RateLimit $e) {
-        // Too many requests made to the API too quickly
-        return response()->json($e->getJsonBody());
-      } catch (\Stripe\Error\InvalidRequest $e) {
-        // Invalid parameters were supplied to Stripe's API
-        return response()->json($e->getJsonBody());
-      } catch (\Stripe\Error\Authentication $e) {
-        // Authentication with Stripe's API failed
-        // (maybe you changed API keys recently)
-        return response()->json($e->getJsonBody());
-      } catch (\Stripe\Error\ApiConnection $e) {
-        // Network communication with Stripe failed
-        return response()->json($e->getJsonBody());
-      } catch (\Stripe\Error\Base $e) {
-        // Display a very generic error to the user, and maybe send
-        // yourself an email
-        return response()->json($e->getJsonBody());
-      } catch (Exception $e) {
-        // Something else happened, completely unrelated to Stripe
-        return response()->json($e->getJsonBody());
-      }
-    }
+    
 }
