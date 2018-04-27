8eb4f25b91d72731f6c48cf3c7ddb2eda287ab4a
Jyrone Parker <jyrone.parker@gmail.com>
2018-04-26 21:00:20 -0400

    limit 25

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -19,7 +19,7 @@ class CustomerController extends Controller
     public function index()
     {
         //
-        return \Stripe\Customer::all(25);
+        return \Stripe\Customer::all(['limit' => 25]);
     }
 
     /**
