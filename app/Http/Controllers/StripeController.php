069cb3ae143461f4ad9ac0fb0711fbbe90e1a9fe
Jyrone Parker <jyrone.parker@gmail.com>
2018-05-07 23:13:35 -0400

    tag

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -60,7 +60,7 @@ class StripeController extends Controller
       }
     }
     public function sendPay(Request $request, $name){
-      $user = \App\User::where('name',$name)->first();
+      $user = \App\User::where('tag',$name)->first();
       $with = [
         'user' => $user
       ];
