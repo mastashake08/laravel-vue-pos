ba94c4101da2b483c83ec5c4aee07e61fca0b6a9
Jyrone Parker <jyrone.parker@gmail.com>
2018-05-10 00:41:35 -0400

    message

--- app/Mail/FrontPageMessage.php
+++ app/Mail/FrontPageMessage.php
@@ -10,15 +10,19 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 class FrontPageMessage extends Mailable
 {
     use Queueable, SerializesModels;
-
+    public $name,$email,$message;
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($name,$email,$message)
     {
         //
+        $this->name = $name;
+        $this->email = $email;
+        $this->message = $message;
+
     }
 
     /**
