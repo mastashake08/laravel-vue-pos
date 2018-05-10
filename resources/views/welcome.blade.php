0f01b4d9913d35b35dbfa2077ac248f6bd85407e
Jyrone Parker <jyrone.parker@gmail.com>
2018-05-10 00:43:34 -0400

    social media

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -114,10 +114,10 @@
 									</ul>
 								</form>
 								<ul class="icons">
-									<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-									<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-									<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-									<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
+									<li><a href="https://twitter.com/mastashake08" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="https://facebook.com/jyroneparker" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+									<li><a href="https://instagram.com/mastashake08" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+									<li><a href="https://github.com/mastashake08" class="icon fa-github"><span class="label">GitHub</span></a></li>
 								</ul>
 							</article>
 					</div>
