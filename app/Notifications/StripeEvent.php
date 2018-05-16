4d8b55eeb4b6e0ea3e592b269c84336b790ddf8b
vagrant <vagrant@homestead>
2018-05-16 01:42:18 +0000

    database notification work

--- app/Notifications/StripeEvent.php
+++ app/Notifications/StripeEvent.php
@@ -31,7 +31,7 @@ class StripeEvent extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['database'];
     }
 
     /**
@@ -59,7 +59,7 @@ class StripeEvent extends Notification
     public function toArray($notifiable)
     {
         return [
-            //
+            'event' => $this->event
         ];
     }
 }
